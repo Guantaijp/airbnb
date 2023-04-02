75b0e81f076ad88fa7808fcde0c1c41454611dbc
GUANTAI001 <jpguantai@gmail.com>
2023-04-02 12:04:05 +0300

    UsersTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_01_075804) do
+ActiveRecord::Schema.define(version: 2023_04_02_090136) do
 
   create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
